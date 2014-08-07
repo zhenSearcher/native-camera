7981405d91984f54202fb129a541ec5014d7af32
Zhen Lu <user@Zhens-MacBook-Pro.local>
2014-08-07 13:58:35 -0700

    Native camera with preview.

---
+++ app/src/main/java/com/left/to/zlu/nativecamera/CameraPreview.java
@@ -0,0 +1,126 @@
+package com.left.to.zlu.nativecamera;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by user on 8/6/14.
+ */
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    // SurfaceHolder
+    private SurfaceHolder mHolder;
+
+    // Our Camera.
+    private Camera mCamera;
+
+    // Camera Sizing (For rotation, orientation changes)
+    private Camera.Size mPreviewSize;
+
+    // List of supported preview sizes
+    private List<Camera.Size> mSupportedPreviewSizes;
+
+    // Flash modes supported by this camera
+    private List<String> mSupportedFlashModes;
+
+    // View holding this camera.
+    private View mCameraView;
+
+    public CameraPreview(Context context, Camera camera) {
+        super(context);
+        setCamera(camera);
+
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        mHolder.setKeepScreenOn(true);
+    }
+
+    /**
+     * Begin the preview of the camera input.
+     */
+    public void startCameraPreview() {
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Extract supported preview and flash modes from the camera.
+     *
+     * @param camera
+     */
+    public void setCamera(Camera camera) {
+        // Source: http://stackoverflow.com/questions/7942378/android-camera-will-not-work-startpreview-fails
+        mCamera = camera;
+        if (mCamera == null) return;
+
+        mSupportedPreviewSizes = mCamera.getParameters().getSupportedPreviewSizes();
+        mSupportedFlashModes = mCamera.getParameters().getSupportedFlashModes();
+
+        // Set the camera to Auto Flash mode.
+        if (mSupportedFlashModes != null && mSupportedFlashModes.contains(Camera.Parameters.FLASH_MODE_AUTO)) {
+            Camera.Parameters parameters = mCamera.getParameters();
+            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_AUTO);
+            mCamera.setParameters(parameters);
+        }
+
+        requestLayout();
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            mCamera.setPreviewDisplay(holder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+
+        if (mHolder.getSurface() == null) {
+            // preview surface does not exist
+            return;
+        }
+
+        // stop preview before making changes
+        try {
+            Camera.Parameters parameters = mCamera.getParameters();
+
+            // Set the auto-focus mode to "continuous"
+            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+
+            // Preview size must exist.
+            if (mPreviewSize != null) {
+                Camera.Size previewSize = mPreviewSize;
+                parameters.setPreviewSize(previewSize.width, previewSize.height);
+            }
+
+            mCamera.setParameters(parameters);
+            mCamera.startPreview();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (mCamera != null) {
+            mCamera.stopPreview();
+        }
+    }
+}
