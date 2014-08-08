afbd7a5379f597c76dfa50b9da20e5d46d391337
Zhen Lu <user@Zhens-MacBook-Pro.local>
2014-08-07 17:03:31 -0700

    Added basic metadata.

--- app/src/main/java/com/left/to/zlu/nativecamera/NativeCameraFragment.java
+++ app/src/main/java/com/left/to/zlu/nativecamera/NativeCameraFragment.java
@@ -3,23 +3,107 @@ package com.left.to.zlu.nativecamera;
 import android.app.Activity;
 import android.app.Fragment;
 import android.hardware.Camera;
+import android.media.ExifInterface;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by user on 8/6/14.
  */
 public class NativeCameraFragment extends Fragment {
 
+    Camera.PictureCallback jpegPictureCallback = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] arg0, Camera arg1) {
+
+            String fileName = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DCIM).toString()
+                    + File.separator
+                    + "PicTest_" + System.currentTimeMillis() + ".jpg";
+            File file = new File(fileName);
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdir();
+            }
+
+            try {
+                BufferedOutputStream bufferedStream = new BufferedOutputStream(
+                        new FileOutputStream(file));
+                bufferedStream.write(arg0);
+                bufferedStream.flush();
+                bufferedStream.close();
+                scanFileToPhotoAlbum(file.getAbsolutePath());
+
+                ExifInterface exif = new ExifInterface(file.getAbsolutePath());
+                exif.setAttribute("UserComment", "Jpeg Caption as comment ...");
+                exif.saveAttributes();
+                ShowExif(exif);
+
+                Toast.makeText(getActivity(),
+                        "[Test] Photo take and store in" + file.toString(),
+                        Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                Toast.makeText(getActivity(), "Picture Failed" + e.toString(),
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    };
+
+    private void ShowExif(ExifInterface exif) {
+        String myAttribute = "Exif information ---\n";
+        myAttribute += getTagString(ExifInterface.TAG_DATETIME, exif);
+        myAttribute += getTagString(ExifInterface.TAG_FLASH, exif);
+        myAttribute += getTagString(ExifInterface.TAG_GPS_LATITUDE, exif);
+        myAttribute += getTagString(ExifInterface.TAG_GPS_LATITUDE_REF, exif);
+        myAttribute += getTagString(ExifInterface.TAG_GPS_LONGITUDE, exif);
+        myAttribute += getTagString(ExifInterface.TAG_GPS_LONGITUDE_REF, exif);
+        myAttribute += getTagString(ExifInterface.TAG_IMAGE_LENGTH, exif);
+        myAttribute += getTagString(ExifInterface.TAG_IMAGE_WIDTH, exif);
+        myAttribute += getTagString(ExifInterface.TAG_MAKE, exif);
+        myAttribute += getTagString(ExifInterface.TAG_MODEL, exif);
+        myAttribute += getTagString(ExifInterface.TAG_ORIENTATION, exif);
+        myAttribute += getTagString(ExifInterface.TAG_WHITE_BALANCE, exif);
+        myAttribute += getTagString("UserComment", exif);
+        metaDataText.setText(myAttribute);
+    }
+
+    private String getTagString(String tag, ExifInterface exif) {
+        return (tag + " : " + exif.getAttribute(tag) + "\n");
+    }
+
+    public void scanFileToPhotoAlbum(String path) {
+
+        MediaScannerConnection.scanFile(getActivity(),
+                new String[]{path}, null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+
+                    public void onScanCompleted(String path, Uri uri) {
+                        Log.i("TAG", "Finished scanning " + path);
+                    }
+                }
+        );
+    }
+
     // Native camera.
     private Camera mCamera;
     // View to display the camera output.
     private CameraPreview mPreview;
+    private TextView metaDataText;
 
     public NativeCameraFragment() {
     }
@@ -35,6 +119,19 @@ public class NativeCameraFragment extends Fragment {
         if (opened == false) {
             Log.d("CameraGuide", "Error, Camera failed to open");
         }
+
+        metaDataText = (TextView) rootView.findViewById(R.id.meta_data);
+
+        Button captureButton = (Button) rootView.findViewById(R.id.camera_button);
+        captureButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // get an image from the camera
+                        mCamera.takePicture(null, null, null, jpegPictureCallback);
+                    }
+                }
+        );
 
         return rootView;
     }
@@ -104,10 +201,18 @@ public class NativeCameraFragment extends Fragment {
                 .getRotation();
         int degrees = 0;
         switch (rotation) {
-            case Surface.ROTATION_0: degrees = 0; break;
-            case Surface.ROTATION_90: degrees = 90; break;
-            case Surface.ROTATION_180: degrees = 180; break;
-            case Surface.ROTATION_270: degrees = 270; break;
+            case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+            case Surface.ROTATION_90:
+                degrees = 90;
+                break;
+            case Surface.ROTATION_180:
+                degrees = 180;
+                break;
+            case Surface.ROTATION_270:
+                degrees = 270;
+                break;
         }
 
         int result;
